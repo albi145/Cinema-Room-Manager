50e889732c3764b772a9b2e1a405a125eb92730c
albi145 <hubertoczapeczka1@gmail.com>
2022-11-13 13:23:48 +0100

    test

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Cinema {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("test");
+
         System.out.println("Enter the number of rows:");
         int rows = scanner.nextInt();
         System.out.println("Enter the number of seats in each row:");
